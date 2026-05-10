931d37f8a99e3d6b4ee778bffb59dfe6305d9466
MiX4401 <ej.radford@outlook.com.au>
2026-05-10 19:11:02 +1000

    .

--- Hosts/ejlaptop/home-manager.nix
+++ Hosts/ejlaptop/home-manager.nix
@@ -11,7 +11,6 @@
             home.username = "${username}";
             home.homeDirectory = "/home/${username}";
             home.stateVersion = "24.05";
-            # programs.home-manager.enable = true; 
         };
     };
 }
