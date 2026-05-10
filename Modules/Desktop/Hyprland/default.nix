ffadcf29f04c9dae147497cb220c80688f601377
MiX4401 <ej.radford@outlook.com.au>
2026-05-10 18:41:01 +1000

    .

--- Modules/Desktop/Hyprland/default.nix
+++ Modules/Desktop/Hyprland/default.nix
@@ -3,8 +3,8 @@
 {
     wayland.windowManager.hyprland = {
         enable = true;
-        package = null;
-        portalPackage = null;
+        # package = null;
+        # portalPackage = null;
         settings = {
             monitor = [ ",preferred,auto,auto" ];
 
@@ -12,8 +12,8 @@
                 gaps_in = 5;
                 gaps_out = 20;
                 border_size = 2;
-                col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-                col.inactive_border = "rgba(595959aa)";
+                # col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+                # col.inactive_border = "rgba(595959aa)";
                 resize_on_border = false; 
                 allow_tearing = false;
                 layout = "dwindle";
@@ -27,7 +27,7 @@
                 drop_shadow = true;
                 shadow_range = 4;
                 shadow_render_power = 3;
-                col.shadow = "rgba(1a1a1aee)";
+                # col.shadow = "rgba(1a1a1aee)";
 
                 blur = {
                     enabled = true;
@@ -37,7 +37,7 @@
             };
 
             animation = { 
-                enabled = true;
+                # enabled = true;
                 bezier = [
                     "myBezier, 0.05, 0.9, 0.1, 1.05"
                 ];
