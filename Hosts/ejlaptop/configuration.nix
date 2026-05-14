201b7cff53d4426fd67302920f95b447446b1e55
MiX4401 <ej.radford@outlook.com.au>
2026-05-14 22:24:03 +1000

    .

--- Hosts/ejlaptop/configuration.nix
+++ Hosts/ejlaptop/configuration.nix
@@ -3,8 +3,6 @@
 {
   imports = [
     ./hardware-configuration.nix
-    # ./home-manager.nix
-    # ./stylix.nix
     ../../Modules/Desktop/default.nix
     ../../Modules/System/base.nix
     ../../Modules/System/users.nix
