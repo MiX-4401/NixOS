b51e17aa0b44cd99163705e21232c90efe95a41c
MiX4401 <ej.radford@outlook.com.au>
2026-05-14 21:10:33 +1000

    .

--- Hosts/ejlaptop/stylix.nix
+++ Hosts/ejlaptop/stylix.nix
@@ -3,8 +3,10 @@
 {
     imports = [ inputs.stylix.nixosModules.stylix ];
     stylix.enable = true;
-    
+
     stylix.targets.hyprland.enable = false;
+
+    stylix.homeManagerIntegration.followSystem = true;
 
     stylix.image = ../../Modules/Desktop/Wallpapers/rainworld2.png;
 }
