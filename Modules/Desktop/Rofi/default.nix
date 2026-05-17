6230654a7a256cef1c058ba9c987d8c0aed012a9
MiX4401 <ej.radford@outlook.com.au>
2026-05-17 22:03:44 +1000

    .

--- Modules/Desktop/Rofi/default.nix
+++ Modules/Desktop/Rofi/default.nix
@@ -1,11 +1,12 @@
 { ... }:
 
 {
-    home.file."file" = {
+    home.file.".config/rofi/myConfig.rasi" = {
         source = ./myConfig.rasi;
     };
 
     programs.rofi = {
         enable = true;
+        configPath = "$XDG_CONFIG_HOME/rofi/myConfig.rasi";
     };
 }
