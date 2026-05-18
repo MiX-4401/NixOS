f5e98ab1fe74919b6dbda49495f95dac8c6efb24
MiX4401 <ej.radford@outlook.com.au>
2026-05-18 22:01:51 +1000

    .

--- Modules/Desktop/Hyprland/default.nix
+++ Modules/Desktop/Hyprland/default.nix
@@ -23,7 +23,7 @@
                 border_size = 2;
                 # "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
                 # "col.inactive_border" = "rgba(595959aa)";
-                "col.active_border" = lib.mkForce "rgb(${config.stylix.base16Scheme.base00})";
+                "col.active_border" = lib.mkForce "rgb(${config.stylix.base16Scheme.base00}) rgba(${config.stylix.base16Scheme.base0D}) 45deg";
                 resize_on_border = false; 
                 allow_tearing = false;
                 layout = "master";
