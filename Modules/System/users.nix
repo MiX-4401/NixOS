f1149a0b2574eb6f76373fc15319bc11a8ce7b6f
MiX4401 <ej.radford@outlook.com.au>
2026-05-27 21:52:22 +1000

    FEAT: updatepassword

--- Modules/System/users.nix
+++ Modules/System/users.nix
@@ -5,6 +5,7 @@
         isNormalUser = true;
         extraGroups = [ "wheel" "libvirtd" ];
         shell = pkgs.zsh;
-        hashedPassword = "$6$dpR5KS22c3tIhRZ7$FhWMuP6mhb4L6BlBkt1BBBb9tMcftiX041KrU/sjazfQYk38YwziumJ1uqJOt/udssACCPvasDmQK2B8oWOwx1";
+        # hashedPassword = "$6$dpR5KS22c3tIhRZ7$FhWMuP6mhb4L6BlBkt1BBBb9tMcftiX041KrU/sjazfQYk38YwziumJ1uqJOt/udssACCPvasDmQK2B8oWOwx1";
+        initialPassword = username;
     };
 }
