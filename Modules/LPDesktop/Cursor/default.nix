11479dc8225ef0cbe6972ccb1a0cb88be6025016
MiX4401 <ej.radford@outlook.com.au>
2026-05-29 22:14:18 +1000

    .

--- Modules/LPDesktop/Cursor/default.nix
+++ Modules/LPDesktop/Cursor/default.nix
@@ -9,4 +9,10 @@
                 x11.enable = true;
                 size = 16;
         };
+
+        xsession.pointCursor = {
+                package = pkgs.breeze-hacked-cursor-theme;
+                name = "Breeze_Hacked";
+                size = 16;
+        };
 }
