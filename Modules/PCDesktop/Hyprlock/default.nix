83adc3fe880bb3f5110f9a3846f999394cd173a5
MiX4401 <ej.radford@outlook.com.au>
2026-05-30 20:30:19 +1000

    .

--- Modules/PCDesktop/Hyprlock/default.nix
+++ Modules/PCDesktop/Hyprlock/default.nix
@@ -21,13 +21,13 @@
         disable_loading_bar = false;
       };
 
-      input-field = lib.mkForce [
-        {
-          position = lib.mkForce "0, 225";
-          halign = lib.mkForce "center";
-          valign = lib.mkForce "bottom";
-        }
-      ];
+    #   input-field = lib.mkForce [
+    #     {
+    #       position = lib.mkForce "0, 225";
+    #       halign = lib.mkForce "center";
+    #       valign = lib.mkForce "bottom";
+    #     }
+    #   ];
 
       image = [
         {
