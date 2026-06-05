168eb4a9f22c57e5238857f79e2a3604b044133b
MiX4401 <ej.radford@outlook.com.au>
2026-06-05 23:06:03 +1000

    .

--- Hosts/ejdesktop/virtualisation.nix
+++ Hosts/ejdesktop/virtualisation.nix
@@ -13,68 +13,124 @@
         enable = true;
     };
 
+    # virtualisation.libvirtd.hooks.qemu = {
+    #     win10 = {
+    #         prepare = {
+    #             begin = pkgs.writeShellScript "Prepare-begin" ''
+    #                 ## Helpful to read output when debugging
+    #                 set -x
+
+    #                 # Stop display manager
+    #                 systemctl stop display-manager.service
+    #                 ## Uncomment the following line if you use GDM
+    #                 #killall gdm-x-session
+
+    #                 # Unbind VTconsoles
+    #                 echo 0 > /sys/class/vtconsole/vtcon0/bind
+    #                 echo 0 > /sys/class/vtconsole/vtcon1/bind
+
+    #                 # Unbind EFI-Framebuffer
+    #                 echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+    #                 # Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
+    #                 sleep 2
+
+    #                 # Unload all AMD  drivers
+    #                 modprobe -r amdgpu
+
+    #                 virsh nodedev-detach pci_0000_2b_00_0
+    #                 virsh nodedev-detach pci_0000_2b_00_1
+
+    #                 ## Load vfio
+    #                 modprobe vfio
+    #                 modprobe vfio_iommu_type1
+    #                 modprobe vfio_pci
+    #             '';
+    #         };
+
+    #         release = {
+    #             end = pkgs.writeShellScript "release-end" ''
+    #                 set -x
+
+    #                 ## Unload vfio
+    #                 modprobe -r vfio_pci
+    #                 modprobe -r vfio_iommu_type1
+    #                 modprobe -r vfio
+
+    #                 virsh nodedev-reattach pci_0000_2b_00_0
+    #                 virsh nodedev-reattach pci_0000_2b_00_1
+
+    #                 # Rebind VT consoles
+    #                 echo 1 > /sys/class/vtconsole/vtcon0/bind
+    #                 echo 1 > /sys/class/vtconsole/vtcon1/bind
+
+    #                 echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+
+    #                 modprobe amdgpu
+
+    #                 # Restart Display Manager
+    #                 systemctl start display-manager.service
+
+    #             '';
+    #         };
+    #     };
+    # };
+
     virtualisation.libvirtd.hooks.qemu = {
-        win10 = {
-            prepare = {
-                begin = pkgs.writeShellScript "Prepare-begin" ''
-                    ## Helpful to read output when debugging
-                    set -x
+        win10-hook = pkgs.writeShellScript "win10-hook" ''
+            set -ex
 
-                    # Stop display manager
-                    systemctl stop display-manager.service
-                    ## Uncomment the following line if you use GDM
-                    #killall gdm-x-session
+            VM="$1"
+            OPERATION="$2"
+            SUBOPERATION="$3"
 
-                    # Unbind VTconsoles
-                    echo 0 > /sys/class/vtconsole/vtcon0/bind
-                    echo 0 > /sys/class/vtconsole/vtcon1/bind
+            if [ "$VM" = "win10" ] &&
+            [ "$OPERATION" = "prepare" ] &&
+            [ "$SUBOPERATION" = "begin" ]; then
 
-                    # Unbind EFI-Framebuffer
-                    echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+            systemctl stop display-manager.service
 
-                    # Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
-                    sleep 2
+            echo 0 > /sys/class/vtconsole/vtcon0/bind || true
+            echo 0 > /sys/class/vtconsole/vtcon1/bind || true
 
-                    # Unload all AMD  drivers
-                    modprobe -r amdgpu
+            echo efi-framebuffer.0 \
+                > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-                    virsh nodedev-detach pci_0000_2b_00_0
-                    virsh nodedev-detach pci_0000_2b_00_1
+            sleep 2
 
-                    ## Load vfio
-                    modprobe vfio
-                    modprobe vfio_iommu_type1
-                    modprobe vfio_pci
-                '';
-            };
+            modprobe -r amdgpu
 
-            release = {
-                end = pkgs.writeShellScript "release-end" ''
-                    set -x
+            virsh nodedev-detach pci_0000_2b_00_0
+            virsh nodedev-detach pci_0000_2b_00_1
 
-                    ## Unload vfio
-                    modprobe -r vfio_pci
-                    modprobe -r vfio_iommu_type1
-                    modprobe -r vfio
+            modprobe vfio
+            modprobe vfio_iommu_type1
+            modprobe vfio_pci
+            fi
 
-                    virsh nodedev-reattach pci_0000_2b_00_0
-                    virsh nodedev-reattach pci_0000_2b_00_1
+            if [ "$VM" = "win10" ] &&
+            [ "$OPERATION" = "release" ] &&
+            [ "$SUBOPERATION" = "end" ]; then
 
-                    # Rebind VT consoles
-                    echo 1 > /sys/class/vtconsole/vtcon0/bind
-                    echo 1 > /sys/class/vtconsole/vtcon1/bind
+            modprobe -r vfio_pci
+            modprobe -r vfio_iommu_type1
+            modprobe -r vfio
 
-                    echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+            virsh nodedev-reattach pci_0000_2b_00_0
+            virsh nodedev-reattach pci_0000_2b_00_1
 
-                    modprobe amdgpu
+            echo 1 > /sys/class/vtconsole/vtcon0/bind || true
+            echo 1 > /sys/class/vtconsole/vtcon1/bind || true
 
-                    # Restart Display Manager
-                    systemctl start display-manager.service
+            echo efi-framebuffer.0 \
+                > /sys/bus/platform/drivers/efi-framebuffer/bind
 
-                '';
-            };
+            modprobe amdgpu
+
+            systemctl start display-manager.service
+            fi
+        '';
         };
-    };
 
     boot.kernelModules = [ "vfio" "vfio_iommu_type1" "vfio_pci" ];
     boot.kernelParams = [ "amd_iommu=on" "iommu=pt" ];
