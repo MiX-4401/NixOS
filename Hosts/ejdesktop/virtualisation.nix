40fa2f6dac210de06dc644908a3a7d513a2c37cf
MiX4401 <ej.radford@outlook.com.au>
2026-06-05 22:36:07 +1000

    UPDATE: more virtualisation

--- Hosts/ejdesktop/virtualisation.nix
+++ Hosts/ejdesktop/virtualisation.nix
@@ -13,17 +13,7 @@
         enable = true;
     };
 
-    # virtualisation.libvirtd = {
-    #     enable = true;
-    #     qemu = {
-    #         package = pkgs.qemu_kvm;
-    #         runAsRoot = true;
-    #         swtpm.enable = true;
-    #     };
-    # };
-
-    # program.virt-manager.enable = true;
-
-    # # boot.kernelModules = [ "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" ];
+    # boot.kernelModules = [ "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" ];
     # boot.kernelModules = [ "amd_iommu=on" "iommu=pt" "video=efifb:off" ];
+    boot.kernelModules = [ "amd_iommu=on" ];
 }
