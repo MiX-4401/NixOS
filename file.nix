1ede7e345682e842351d217bd93b89ac3a86fcd5
MiX4401 <ej.radford@outlook.com.au>
2026-06-12 23:44:47 +1000

    .

--- file.nix
+++ file.nix
@@ -1,26 +1,63 @@
+# Knowledge base
+
+# Functiton for creating new hosts within the flake.nix
 # { ... }:
 
-# let
-#     sshSettings = {
-#         ports = [ 10, 11 ];
-#     };
-# in
 # {
-#     services.openssh = (sshSettings // { enable = true; });
+#     # Function for creating new hosts
+#     # Take: hostname, system architecture, file imports
+#     # Return: New host build using lib.nixosSystem grabbed from nixpkgs
+#     # Notes: 
+#     # - Autodefines hostname using nix option 'networking.hostName'
+#     nixosConfigurations = let
+#         makeNewHost = { hostname, system, modules }: nixpkgs.lib.nixosSystem {
+#             inherit system;
+#             modules = modules ++ [{ networking.hostName = hostname }];
+#         };
+#     in 
+    
+#     {
+#         ejdesktop = makeNewHost {
+#             hostname = "ejdesktop";
+#             system = "x86_64-linux";
+#             modules = [ ./Hosts/ejdesktop/configuration.nix ];
+#         };
+#     };
 # }
 
-{ ... }:
+# Merging sets which are defined multiple times
+# { pkgs, lib, ... }:
 
-{
-    services.openssh = let 
-        setPorts = myports: {
-            ports = myports;
-            settings.PermitRootLogin = true;
-            settings.AllowUsers = false;
-        };
-    in
+# {
+#     environment.systemPackages = lib.mkBefore [
+#         # These packages come first
+#         # ...
+#     ];  
 
-    {
-        
-    };
-}
+#     environment.systemPackges = [
+#         # These packages come after
+#         # ...
+#     ]
+
+#     # All packages are concrecated/merged at the end
+# }
+
+# Installing software conditionally
+
+# configuration.nix
+# { config, pkgs, ... }:    # Config parameter means to reviewer sets from other modules
+
+# {
+#     environemnt.systemPackages = if config.programs.hyprland.enable then [
+#         pkgs.waybar
+#     ];
+# }
+
+# # file1.nix
+# { config, ... }:
+
+# {
+#     programs.hyprland.enable = true;
+# }
+
+# 
