048b846a0a22a0ae864292ec5498206b392e30f2
MiX4401 <ej.radford@outlook.com.au>
2026-06-15 23:28:35 +1000

    .

--- Modules/Desktop/DesktopV2/PackageBundles/Social/default.nix
+++ Modules/Desktop/DesktopV2/PackageBundles/Social/default.nix
@@ -1,13 +1,21 @@
 { lib, config, pkgs, username, ... }:
 
 {
+
+    imports = [
+        (lib.mkAliasOptionModule
+            [ "desktopPackageBundleSocials" ]
+            [ "home-manager" "users" "ejradford" "desktopPackageBundleSocials" ]
+        )
+    ];
+
     options.desktopPackageBundleSocials.enable = lib.mkOption {
         type = lib.types.bool;
         default = false;
         description = "Set the package bundle containing software relating to socials";
     };
 
-    config = lib.mkIf home-manager.user.${username}.desktopPackageBundleSocials.enable {
+    config = lib.mkIf config.desktopPackageBundleSocials.enable {
         home.packages = with pkgs; [
             discord
             spotify        
