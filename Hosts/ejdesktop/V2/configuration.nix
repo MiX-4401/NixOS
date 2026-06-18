0e7b89dc34c97f604b91ae5e7b572f8f8b400d81
MiX4401 <ej.radford@outlook.com.au>
2026-06-18 20:11:19 +1000

    .

--- Hosts/ejdesktop/V2/configuration.nix
+++ Hosts/ejdesktop/V2/configuration.nix
@@ -23,8 +23,8 @@
     desktopGit.enable = true;
     desktopGit.username = "MiX4401";
     desktopGit.email = "ej.radford@outlook.com.au";
-    desktopSetWallpaper.monitors = [ "HDMI-A-1,1920x1080@75,auto,1.2" "DP-2,1920x1080@144,0x0,1.2" ]; # Right, left monitors
-    dekstopHyprpaper.wallpaper = "rainworld1.png";
+    desktopHyprland.monitors = [ "HDMI-A-1,1920x1080@75,auto,1.2" "DP-2,1920x1080@144,0x0,1.2" ]; # Right, left monitors
+    desktopSetWallpaper.wallpaper = "rainworld1.png";
     desktopPackageBundleDesktopUtilities.enable = true;
     desktopPackageBundleExtra.enable = true;
     desktopPackageBundleGaming.enable = true;
