1c72dd3f96869faebdb2ff3fd703549053689953
MiX4401 <ej.radford@outlook.com.au>
2026-06-18 20:16:35 +1000

    .

---
+++ Hosts/ejlaptop/V1/configuration.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    # Host unique
+    ./hardware-configuration.nix
+
+    # System immutable
+    ../../Modules/System/bootstrap.nix
+    ../../Modules/LPDesktop/default.nix
+    
+    # System mutable
+    ../../Modules/System/Base/moduleBundle.nix
+    ../../Modules/System/Security/moduleBundle.nix
+  ];
+
+  # Base settings
+  BaseSetAllowUnfreeSoftware.enable = true;
+  BaseSetHostname.hostname = "ejlaptop";
+  BaseSetOSVersion.version = "24.05";
+  BaseSetGarbageCollection.enable = true;
+  BaseSetDownloadBufferSize.size = 134217728;
+
+  # Security settings
+  SecurityHardenFirewall.enable = true;
+  SecurityHardenSSH.enable = false;
+  SecurityHardenRoot.enable = true;
+  SecurityHardenSudo.enable = true;
+
+  # Host specific global packages
+  environment.systemPackages = with pkgs; [
+    microcode-intel
+    sof-firmware
+  ];
+}
