c7c7d0adb9df684fe556a79d6789af4a0d6da748
MiX4401 <ej.radford@outlook.com.au>
2026-06-23 20:44:53 +1000

    .

--- Modules/Desktop/DesktopV2/Packages/Walker/default.nix
+++ Modules/Desktop/DesktopV2/Packages/Walker/default.nix
@@ -1,10 +1,26 @@
 { pkgs, ... }: 
 
 {
-    home.packages = with pkgs; [ walker ];
+    home.packages = with pkgs; [ walker elephant ];
     services.walker = {
         enable = true;
         systemd.enable = true;
+        enableElephantIntegration = true;
+
+        # settings = {
+        #     app_launch_prefix = "";
+        #     as_window = false;
+        #     close_when_open = false;
+        #     disable_click_to_close = false;
+        #     force_keyboard_focus = true;
+        #     terminal_title_flag = "";
+        #     theme = "default";
+        #     timeout = 20;
+        # };
+    };
+
+    services.elephant = {
+        enable = true;
 
         # settings = {
         #     app_launch_prefix = "";
