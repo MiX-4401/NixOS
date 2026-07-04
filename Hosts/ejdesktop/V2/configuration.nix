849ec0fed011794936b3c8fca68c6bd2aa0bbb21
MiX4401 <ej.radford@outlook.com.au>
2026-07-05 01:17:43 +1000

    .

--- Hosts/ejdesktop/V2/configuration.nix
+++ Hosts/ejdesktop/V2/configuration.nix
@@ -26,10 +26,10 @@
     core.system.nixos.garbageCollection.period = "daily";
     core.system.nixos.osVersion = "26.05";
     core.system.misc.gaming.enable = true;
-    core.system.nixos.hibernation.enable = true;
-    core.system.nixos.hibernation.hibernateDevice = "/dev/sdc3";
-    core.system.nixos.hibernation.hibernateAfter = "2min";
-    core.system.nixos.hibernation.sleepAfter = "1min";
+    # core.system.nixos.hibernation.enable = true;
+    # core.system.nixos.hibernation.hibernateDevice = "/dev/sdc3";
+    # core.system.nixos.hibernation.hibernateAfter = "2min";
+    # core.system.nixos.hibernation.sleepAfter = "1min";
 
     # Core desktop modular settings
 
