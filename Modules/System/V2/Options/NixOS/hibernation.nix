089263a5c522c96f12c56fcd03ef3e3e99a09795
MiX4401 <ej.radford@outlook.com.au>
2026-07-05 00:38:11 +1000

    .

--- Modules/System/V2/Options/NixOS/hibernation.nix
+++ Modules/System/V2/Options/NixOS/hibernation.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 
 {
 
@@ -8,14 +8,23 @@
             default = false;
             description = "Enable system hibernation";
         };
+        
         hibernateDevice = lib.mkOption {
             type = lib.types.str;
             description = "The device (swap) which will handle hibernation"; 
+        };
+
+        hibernateAfter = lib.mkOption {
+            type = lib.types.str;
+            default = "1h";
+            description = "How long after sleeping should the system hibernate (1h)";
         }
     };
 
 
     config = lib.mkIf config.core.system.nixos.hibernation.enable {
+        
+        # Pulled from https://wiki.nixos.org/wiki/Power_Management#Hibernation (I do not know how it works)
         services.udev.extraRules = 
         let
             mkRule = as: lib.concatStringsSep ", " as;
@@ -28,7 +37,22 @@
             ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 41 /dev/%k"''
         ])]);
 
-        swapDevices = [{ device = config.core.system.nixos.hibernatDevice; }];
+        # Hibernate device to resume from 
+        swapDevices = [{ device = config.core.system.nixos.hibernatDevice; }]; 
         boot.resumeDevice = config.core.system.nixos.hibernatDevice;
+
+        # Allow sleep
+        systemd.sleep.settings.Sleep = {
+            AllowSuspend = "yes";
+            AllowHibernation = "yes";
+            HibernateDelaySec = "10min";
+            AllowHybridSleep = "yes";
+            AllowSuspendThenHibernate = "yes";
+        };
+
+        services.logind.extraConfig = ''
+            IdleAction=suspend
+            IdleActionSec=5min
+        '';
     };
 }
