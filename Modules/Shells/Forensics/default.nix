953caae347318050a5a6b4e9e23711fa36759c8d
MiX4401 <ej.radford@outlook.com.au>
2026-07-14 22:24:59 +1000

    .

--- Modules/Shells/Forensics/default.nix
+++ Modules/Shells/Forensics/default.nix
@@ -2,6 +2,14 @@
 
 pkgs.mkShell {
     packages = with pkgs; [
-        file
+        file        # Print file metadata
+        unzip       # Extract compressed files in format .zip 
+        gnutar      # Extract compressed files in format .tar 
+        gzip        # Extract compressed files in format .gz 
+        zstd        # Extract compressed files in format .zstd 
+	    yazi	    # TUI file manager
+        xxd         # Hex dump viewer
+        libewf      # Forensics tool suite
+        fleuthkit   # Forensics tool suite
     ];
 }
