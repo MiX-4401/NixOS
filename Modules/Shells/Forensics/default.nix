a7d682074a8a290203bb8dbcb35d7dd4d8ef1302
MiX4401 <ej.radford@outlook.com.au>
2026-07-20 14:50:33 +1000

    .

--- Modules/Shells/Forensics/default.nix
+++ Modules/Shells/Forensics/default.nix
@@ -2,14 +2,17 @@
 
 pkgs.mkShell {
     packages = with pkgs; [
-        file        # Print file metadata
+        libewf      # Forensics tool suite
+        sleuthkit   # Forensics tool suite
+        testdisk    # Data recovery tool suite
         unzip       # Extract compressed files in format .zip 
         gnutar      # Extract compressed files in format .tar 
         gzip        # Extract compressed files in format .gz 
         zstd        # Extract compressed files in format .zstd 
+        _7zz        # Extract compressed files in format .7z 
 	    yazi	    # TUI file manager
         xxd         # Hex dump viewer
-        libewf      # Forensics tool suite
-        sleuthkit   # Forensics tool suite
+        file        # Print file metadata
+        bc          # TUI calculator
     ];
 }
