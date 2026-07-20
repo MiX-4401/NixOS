57dda85add2ee52a3ef0627979f59a94233edb29
MiX4401 <ej.radford@outlook.com.au>
2026-07-21 02:08:24 +1000

    .

--- Modules/Desktop/V2/Packages/Hyprland/Configs/keybinds.lua
+++ Modules/Desktop/V2/Packages/Hyprland/Configs/keybinds.lua
@@ -31,11 +31,14 @@ hl.bind("SUPER + 8", hl.dsp.focus({ workspace = "8" }))
 hl.bind("SUPER + 9", hl.dsp.focus({ workspace = "9" }))
 
 -- Window management
-hl.bind("SUPER + Q",            hl.dsp.window.close())
-hl.bind("SUPER + SHIFT + Q",    hl.dsp.window.kill())
-hl.bind("SUPER + F",            hl.dsp.window.fullscreen())
--- hl.bind("SUPER + RETURN",       hl.dsp.window.swap({ w }))
-hl.bind("SUPER + V",            hl.dsp.window.float())
+hl.bind("SUPER + Q",                    hl.dsp.window.close())
+hl.bind("SUPER + SHIFT + Q",            hl.dsp.window.kill())
+hl.bind("SUPER + F",                    hl.dsp.window.fullscreen())
+-- hl.bind("SUPER + RETURN",            hl.dsp.window.swap({ w }))
+hl.bind("SUPER + V",                    hl.dsp.window.float())
+hl.bind("SUPER + mouse:272",            hl.dsp.window.drag(),   { mouse = true })
+hl.bind("SUPER + SHIFT + mouse:272",    hl.dsp.window.resize(), { mouse = true })
+
 
 hl.bind("SUPER + SHIFT + 1", hl.dsp.window.move({ workspace = "1", follow = false }))
 hl.bind("SUPER + SHIFT + 2", hl.dsp.window.move({ workspace = "2", follow = false }))
@@ -48,24 +51,12 @@ hl.bind("SUPER + SHIFT + 8", hl.dsp.window.move({ workspace = "8", follow = fals
 hl.bind("SUPER + SHIFT + 9", hl.dsp.window.move({ workspace = "9", follow = false }))
 
 -- Hardware keys
-hl.bind("XF86MonBrightnessUp",
-    hl.dsp.exec_cmd("brightnessctl set +10%"))
-
-hl.bind("XF86MonBrightnessDown",
-    hl.dsp.exec_cmd("brightnessctl set 10%-"))
-
-hl.bind("XF86AudioRaiseVolume",
-    hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"))
-
-hl.bind("XF86AudioLowerVolume",
-    hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"))
-
-hl.bind("XF86AudioMute",
-    hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"))
-
-hl.bind("XF86AudioMicMute",
-    hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"))
-
+hl.bind("XF86MonBrightnessUp",      hl.dsp.exec_cmd("brightnessctl set +10%"))
+hl.bind("XF86MonBrightnessDown",    hl.dsp.exec_cmd("brightnessctl set 10%-"))
+hl.bind("XF86AudioRaiseVolume",     hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"))
+hl.bind("XF86AudioLowerVolume",     hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"))
+hl.bind("XF86AudioMute",            hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"))
+hl.bind("XF86AudioMicMute",         hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"))
 -- Mouse wheel workspaces
 -- hl.bind("SUPER + mouse_down", hl.workspace("e+1"))
 -- hl.bind("SUPER + mouse_up", hl.workspace("e-1"))
