a9756f12cba4f2ddc163fb5ebe94a5010256e3e9
MiX4401 <ej.radford@outlook.com.au>
2026-07-21 02:27:01 +1000

    .

--- Modules/Desktop/V2/Packages/Hyprland/Configs/decorations.lua
+++ Modules/Desktop/V2/Packages/Hyprland/Configs/decorations.lua
@@ -56,5 +56,21 @@ hl.config({
     misc = {
         animate_manual_resizes = true;
         animate_mouse_windowdragging = true;
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+    };
+
+    xwayland = {
+        enabled = true;
+        force_zero_scaling = true;
+    };
+
+    render = {
+        direct_scanout = 1;
+    };
+
+    ecosystem = {
+        no_update_news = true;
+        no_donation_nag = true;
     };
 })
