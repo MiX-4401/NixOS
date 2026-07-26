dd0db7ffecd5b8e4847ad56d75bac61f01b6f50f
MiX4401 <ej.radford@outlook.com.au>
2026-07-26 20:39:01 +1000

    .

--- Hosts/ejvm/V1/disko-config.nix
+++ Hosts/ejvm/V1/disko-config.nix
@@ -12,39 +12,72 @@
 
     disko.devices = {
         disk = {
-            main = {
-                type = "disk";
-                device = "/dev/vda";
-                content = {
-                    type = "gpt";
-                    partitions = {
-                        ESP = {
-                            size = "1G";
-                            type = "EF00";
+            type = "disk";
+            device = "/dev/vda";
+            content = {
+                type = "table";
+                format = "gpt";
+                partitions = [
+
+                    # Boot partition
+                    {
+                        name = "ESP";
+                        size = "500M";
+                        type = "EF00";
+                        content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "umask=0077" ];
+                        };
+                    }
+
+                    # LUKs partition (root)
+                    {
+                        name = "root";
+                        size = "100%";
+                        content = {
+                            type = "luks";
+                            name = "cryptroot";
                             content = {
-                                type = "filesystem";
-                                format = "vfat";
-                                mountpoint = "/boot";
-                                mountOptions = [ "umask=0077" ];
+                                type = "lvm_pv";
+                                vg = "myPool";
                             };
                         };
-                        luks = {
-                            size = "100%";
-                            content = {
-                                type = "luks";
-                                name = "crypted";
-                                settings.allowDiscards = true;
-                                passwordFile = "/tmp/secret.key";
-                                content = {
-                                    type = "filesystem";
-                                    format = "ext4";
-                                    mountpoint = "/";
-                                };
-                            };
-                        };
-                    };
-                };
+                    }
+                ];
             };
+        };
+
+        lvm_vg.myPool = {
+            type = "lvm_vg";
+            lvs = [
+
+                # SWAP
+
+                {
+                    name = "swap";
+                    size = "8G";
+                    content = {
+                        type = "swap";
+                        discardPolicy = "both";
+                        resumeDevice = true;
+                    };
+                }
+
+                # Root
+                {
+                    name = "root";
+                    size = "100%FREE";
+                    content = {
+                        type = "filesystem";
+                        format = "ext4";
+                        mountpoint = "/";
+                        mountOptions = [ "defaults" ];
+                    };
+                }
+
+            ];
         };
     };
 }
