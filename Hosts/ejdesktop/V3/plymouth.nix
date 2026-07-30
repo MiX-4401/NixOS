7ea74fbef5c385582652155e89aa4831ee49dab8
MiX4401 <ej.radford@outlook.com.au>
2026-07-30 22:07:33 +1000

    .

--- Hosts/ejdesktop/V3/plymouth.nix
+++ Hosts/ejdesktop/V3/plymouth.nix
@@ -3,14 +3,19 @@
 {
     boot = {
         plymouth = {
-            enable = true;
+            enable = lib.mkForce true;
+            theme = lib.mkForce "spinner";
         };
+
+        initrd.systemd.enable = true;
 
         # Enable "Silent boot"
         consoleLogLevel = 3;
         initrd.verbose = false;
         kernelParams = [
             "quiet"
+            "splash"
+            # "boot.shell_on_fail"
             "rd.udev.log_level=3"
             "rd.systemd.show_status=auto"
         ];
