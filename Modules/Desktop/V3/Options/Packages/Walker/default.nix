9dd504acc7a3bdd8740da234d4940bfe298a413a
MiX4401 <ej.radford@outlook.com.au>
2026-08-03 16:06:19 +1000

    .

--- Modules/Desktop/V3/Options/Packages/Walker/default.nix
+++ Modules/Desktop/V3/Options/Packages/Walker/default.nix
@@ -3,16 +3,16 @@
 {
     home.packages = with pkgs; [ walker elephant ];
     services.walker = {
-        enable = false;
-        systemd.enable = false;
-        enableElephantIntegration = false;
+        enable = true;
+        systemd.enable = true;
+        enableElephantIntegration = true;
 
         # settings = {
         #     app_launch_prefix = "";
         #     as_window = false;
         #     close_when_open = false;
         #     disable_click_to_close = false;
-        #     force_keyboard_focus = false;
+        #     force_keyboard_focus = true;
         #     terminal_title_flag = "";
         #     theme = "default";
         #     timeout = 20;
@@ -20,54 +20,17 @@
     };
 
     services.elephant = {
-        enable = false;
+        enable = true;
 
         # settings = {
         #     app_launch_prefix = "";
         #     as_window = false;
         #     close_when_open = false;
         #     disable_click_to_close = false;
-        #     force_keyboard_focus = false;
+        #     force_keyboard_focus = true;
         #     terminal_title_flag = "";
         #     theme = "default";
         #     timeout = 20;
         # };
     };
 }
-
-# { pkgs, ... }: 
-
-# {
-#     home.packages = with pkgs; [ walker elephant ];
-#     services.walker = {
-#         enable = true;
-#         systemd.enable = true;
-#         enableElephantIntegration = true;
-
-#         # settings = {
-#         #     app_launch_prefix = "";
-#         #     as_window = false;
-#         #     close_when_open = false;
-#         #     disable_click_to_close = false;
-#         #     force_keyboard_focus = true;
-#         #     terminal_title_flag = "";
-#         #     theme = "default";
-#         #     timeout = 20;
-#         # };
-#     };
-
-#     services.elephant = {
-#         enable = true;
-
-#         # settings = {
-#         #     app_launch_prefix = "";
-#         #     as_window = false;
-#         #     close_when_open = false;
-#         #     disable_click_to_close = false;
-#         #     force_keyboard_focus = true;
-#         #     terminal_title_flag = "";
-#         #     theme = "default";
-#         #     timeout = 20;
-#         # };
-#     };
-# }
