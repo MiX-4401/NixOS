79f9caa9178c7f6389e4f79d19702a2bad440ee5
MiX4401 <ej.radford@outlook.com.au>
2026-08-03 16:00:47 +1000

    .

--- Modules/Desktop/V3/Options/Packages/Walker/default.nix
+++ Modules/Desktop/V3/Options/Packages/Walker/default.nix
@@ -3,16 +3,16 @@
 {
     home.packages = with pkgs; [ walker elephant ];
     services.walker = {
-        enable = true;
-        systemd.enable = true;
-        enableElephantIntegration = true;
+        enable = false;
+        systemd.enable = false;
+        enableElephantIntegration = false;
 
         # settings = {
         #     app_launch_prefix = "";
         #     as_window = false;
         #     close_when_open = false;
         #     disable_click_to_close = false;
-        #     force_keyboard_focus = true;
+        #     force_keyboard_focus = false;
         #     terminal_title_flag = "";
         #     theme = "default";
         #     timeout = 20;
@@ -20,17 +20,54 @@
     };
 
     services.elephant = {
-        enable = true;
+        enable = false;
 
         # settings = {
         #     app_launch_prefix = "";
         #     as_window = false;
         #     close_when_open = false;
         #     disable_click_to_close = false;
-        #     force_keyboard_focus = true;
+        #     force_keyboard_focus = false;
         #     terminal_title_flag = "";
         #     theme = "default";
         #     timeout = 20;
         # };
     };
 }
+
+# { pkgs, ... }: 
+
+# {
+#     home.packages = with pkgs; [ walker elephant ];
+#     services.walker = {
+#         enable = true;
+#         systemd.enable = true;
+#         enableElephantIntegration = true;
+
+#         # settings = {
+#         #     app_launch_prefix = "";
+#         #     as_window = false;
+#         #     close_when_open = false;
+#         #     disable_click_to_close = false;
+#         #     force_keyboard_focus = true;
+#         #     terminal_title_flag = "";
+#         #     theme = "default";
+#         #     timeout = 20;
+#         # };
+#     };
+
+#     services.elephant = {
+#         enable = true;
+
+#         # settings = {
+#         #     app_launch_prefix = "";
+#         #     as_window = false;
+#         #     close_when_open = false;
+#         #     disable_click_to_close = false;
+#         #     force_keyboard_focus = true;
+#         #     terminal_title_flag = "";
+#         #     theme = "default";
+#         #     timeout = 20;
+#         # };
+#     };
+# }
--- Modules/System/V2/bootstrap.nix
+++ Modules/System/V2/bootstrap.nix
@@ -63,11 +63,8 @@
         htop.enable = true;     # Simple TUI resource monitor
         nano.enable = true;     # Simple TUI text editor
 
-        hyprland.enable = true; # GUI Desktop environment
-        # hyprland.package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.default;
-        # hyprland.portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.default;
-        
-        hyprland.withUWSM = true;        # Added
-        hyprland.xwayland.enable = true; # Added
+        hyprland.enable = true; # GUI Desktop environment  
+        hyprland.withUWSM = true;       
+        hyprland.xwayland.enable = true; 
     };
 }
