2833d9d84459f10a567d738313ef0f150954403a
MiX4401 <ej.radford@outlook.com.au>
2026-08-11 19:46:09 +1000

    .

--- Modules/System/V2/Options/Security/hardenAuditing.nix
+++ Modules/System/V2/Options/Security/hardenAuditing.nix
@@ -8,19 +8,22 @@
             description = "Enable auditing";
         };
 
-        monitor = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [ "/etc/nixos" ];
-            description = "A list of monitored paths";
-        };
+        # monitor = lib.mkOption {
+        #     type = lib.types.listOf lib.types.str;
+        #     default = [ "/etc/nixos" ];
+        #     description = "A list of monitored paths";
+        # };
     };
 
     # Hardening firewall settings
-    config = config.core.system.security.hardenAuditing.enable {
+    config = lib.mkIf config.core.system.security.hardenAuditing.enable {
         
-        services.auditd = {
-            enable = "lock"; # Prevent root from removing logs
+        services.journald.audit = true;
+        security.auditd.enable = true;
+        security.audit = {
+            enable = "lock";
             rules = [
+
                 # Log changes to important files
                 "-w /etc/passwd -p wa -k passwd_changes"
                 "-w /etc/shadow -p wa -k shadow_changes"
@@ -35,14 +38,12 @@
                 "-a always,exit -F arch=b64 -S open,openat -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access_denied"
 
                 # Log file creation events
-                "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rmdir -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k user_deletedFile"
+                # "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rmdir -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k user_deletedFile"
 
                 # Log file deletion events
-                "-a always,exit -F arch=b64 -S creat -S open -S openat -S unlinkat -F auid>=1000 -F auid!=4294967295 -k user_createdFile"
+                # "-a always,exit -F arch=b64 -S creat -S open -S openat -S unlinkat -F auid>=1000 -F auid!=4294967295 -k user_createdFile"
 
             ];
         };
-
-        services.journald.audit = true;
     };
 }
